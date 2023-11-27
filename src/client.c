8ed39b19b28f72a487f53f1de6928378925576a4
Dima Klimkin <dklimkin@z1t2c4.42bangkok.com>
2023-11-27 12:39:34 +0700

    feat: add bit receival protection

--- src/client.c
+++ src/client.c
@@ -12,7 +12,50 @@
 
 #include "../includes/minitalk.h"
 
-static void	send_message(int pid, char *message)
+static int	g_is_signal_acknowledged;
+
+static void	handle_signal_acknowledgement(int signum)
+{
+	(void)signum;
+	g_is_signal_acknowledged = TRUE;
+}
+
+static void	send_client_pid(int server_pid)
+{
+	int	cleint_pid;
+	int	i;
+
+	cleint_pid = getpid();
+	i = 31;
+	while (i >= 0)
+	{
+		if ((cleint_pid >> i) & 1)
+			kill(server_pid, SIGUSR2);
+		else
+			kill(server_pid, SIGUSR1);
+		usleep(500);
+		i--;
+	}
+}
+
+static void	send_bit(int server_pid, int bit)
+{
+	while (1)
+	{
+		if (bit == '1')
+			kill(server_pid, SIGUSR2);
+		if (bit == '0')
+			kill(server_pid, SIGUSR1);
+		usleep(100);
+		if (g_is_signal_acknowledged)
+		{
+			g_is_signal_acknowledged = FALSE;
+			break ;
+		}
+	}
+}
+
+static void	send_message(int server_pid, char *message)
 {
 	char	c;
 	char	bit;
@@ -28,10 +71,10 @@ static void	send_message(int pid, char *message)
 		while (k >= 0)
 		{
 			if ((c >> k) & 1)
-				kill(pid, SIGUSR2);
+				send_bit(server_pid, '1');
 			else
-				kill(pid, SIGUSR1);
-			usleep(500);
+				send_bit(server_pid, '0');
+			usleep(100);
 			k--;
 		}
 		i++;
@@ -40,22 +83,28 @@ static void	send_message(int pid, char *message)
 
 int	main(int argc, char **argv)
 {
-	char	*message;
-	int		pid;
+	struct sigaction	sa;
+	char				*message;
+	int					server_pid;
 
+	ft_printf("Client PID: %d\n\n", getpid());
 	if (argc != 3)
 	{
 		ft_printf("please provide PID and a string\n");
 		exit(1);
 	}
-	pid = ft_atoi(argv[1]);
-	if (!pid)
+	server_pid = ft_atoi(argv[1]);
+	if (!server_pid)
 	{
-		ft_printf("please provide correct PID\n");
+		ft_printf("please provide correct server PID\n");
 		exit (1);
 	}
 	message = argv[2];
+	send_client_pid(server_pid);
+	sa.sa_flags = 0;
+	sa.sa_handler = handle_signal_acknowledgement;
+	sigaction(SIGUSR1, &sa, NULL);
 	ft_printf("client says:\n\n%s ⸜(ˆᗜˆ˵ )⸝\n\n\n", message);
-	send_message(pid, message);
+	send_message(server_pid, message);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -12,31 +12,70 @@
 
 #include "../includes/minitalk.h"
 
-static void	handle_signal(int signum)
+static int	g_client_pid;
+
+static void	handle_signal_message(int signum)
 {
 	static char	c;
-	static int	bit;
+	static int	bits;
 
 	c = c | (signum == SIGUSR2);
-	bit++;
-	if (bit == 8)
+	bits++;
+	if (bits == 8)
 	{
 		write(1, &c, 1);
-		bit = 0;
+		bits = 0;
 		c = 0;
 	}
 	else
 		c <<= 1;
+	kill(g_client_pid, SIGUSR1);
+}
+
+static void	get_client_pid(int signum)
+{
+	struct sigaction	sa;
+	static int			bits;
+
+	g_client_pid |= (signum == SIGUSR2);
+	bits++;
+	if (bits == 32)
+	{
+		ft_printf("Client PID: %d\n", g_client_pid);
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = 0;
+		sa.sa_handler = handle_signal_message;
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+	}
+	else
+		g_client_pid <<= 1;
 }
 
 int	main(void)
 {
-	int	pid;
+	struct sigaction	sa;
+	int					server_pid;
+	int					client_pid;
 
-	pid = getpid();
-	ft_printf("˖⁺｡˚⋆˙₊˚✧  ੈ‧₊˚*‧.₊˚\n\nPID: %d | ( ᵔ ᵕ ᵔ) ", pid);
-	signal(SIGUSR1, handle_signal);
-	signal(SIGUSR2, handle_signal);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = get_client_pid;
+	server_pid = getpid();
+	ft_printf("˖⁺｡˚⋆˙₊˚✧  ੈ‧₊˚*‧.₊˚\n\nServer PID: %d | ( ᵔ ᵕ ᵔ) ", server_pid);
+	client_pid = 0;
+	if (sigaction(SIGUSR1, &sa, NULL) == ERROR)
+	{
+		write(STDERR_FILENO, "Error setting SIGUSR1 handler", 29);
+		return (1);
+	}
+	if (sigaction(SIGUSR2, &sa, NULL) == ERROR)
+	{
+		write(STDERR_FILENO, "Error setting SIGUSR2 handler", 29);
+		return (1);
+	}
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
 	return (0);
